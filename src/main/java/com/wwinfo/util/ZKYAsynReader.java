a6e58de56c655905e632b5eaee36239ee0776693
lk <1973711398@qq.com>
2022-06-21 15:29:35 +0800

    修改

--- src/main/java/com/wwinfo/util/ZKYAsynReader.java
+++ src/main/java/com/wwinfo/util/ZKYAsynReader.java
@@ -2,8 +2,8 @@ package com.wwinfo.util;
 
 import java.util.HashMap;
 
-import lombok.extern.slf4j.Slf4j;
 import com.uhf.api.cls.Reader;
+import lombok.extern.slf4j.Slf4j;
 import com.uhf.api.cls.Reader.AntPortsVSWR;
 import com.uhf.api.cls.Reader.Mtr_Param;
 import com.uhf.api.cls.Reader.READER_ERR;
@@ -83,7 +83,7 @@ public class ZKYAsynReader {
 		@Override
 		public void run(){
 			//启动线程异步读取
-			devReader = new Reader();
+			/*devReader = new Reader();
 			READER_ERR err;
 			int[] antsTemp = new int[4];
 			while(true){
@@ -112,7 +112,7 @@ public class ZKYAsynReader {
 			    	apvr.vswrs[0].frequency = 915250;
 			    	err=devReader.ParamGet(Mtr_Param.MTR_PARAM_RF_ANTPORTS_VSWR, apvr);
 			    	//antID1234即对应物理天线口1234，当获取的vswr小于10时即可认为天线口有天线连接
-			    	System.out.println(" antID = "+i+" vswr= "+apvr.vswrs[0].vswr+" frequency= "+apvr.vswrs[0].frequency);
+			    	//System.out.println(" antID = "+i+" vswr= "+apvr.vswrs[0].vswr+" frequency= "+apvr.vswrs[0].frequency);
 					if (err != READER_ERR.MT_OK_ERR){
 			        	log.error("RFID ParamGet for andid="+Integer.toString(i)+" failed：" + err.toString()+"("+m_ip+")");
 					}else if (apvr.vswrs[0].vswr < 10){
@@ -139,7 +139,7 @@ public class ZKYAsynReader {
         	//log.debug("start RFID read：ip=" + m_ip+";天线数："+Integer.toString(m_antCount));
         	log.info("start RFID read：ip=" + m_ip+";天线数："+Integer.toString(m_antCount));
 			while(true){
-				TAGINFO[] tags=new TAGINFO[200];
+				*//*TAGINFO[] tags=new TAGINFO[200];
 				int[] tagnum=new int[1];
 				short timeout = (short)(200*m_antCount);
 				err = devReader.TagInventory(m_ants,m_antCount, timeout, tags, tagnum);
@@ -168,8 +168,159 @@ public class ZKYAsynReader {
 					Thread.sleep(500);
 				}catch(Exception e){
 					e.printStackTrace();
+				}*//*
+
+				//6/19
+				TAGINFO[] tags = new TAGINFO[200];
+				int[] tagnum = new int[1];
+				short timeout = 1000; // 1秒
+				// err = devReader.TagInventory(cast.connectedants, antCount, timeout, tags,
+				// tagnum);
+				// err = devReader.TagInventory(ants,antCount, timeout, tags, tagnum);
+				// 2022年6月17日,使用新的盘点接口
+				err = devReader.TagInventory_Raw(m_ants, m_antCount, timeout, tagnum);
+				if (err != READER_ERR.MT_OK_ERR) {
+					System.out.println("RFID TagInventory failed：" + err.toString() + "(" + m_ip + ")");
+					return;
 				}
-			}
+			*//*if (tagnum[0] < 1)
+				return;*//*
+				if(tagnum[0] > 0){
+					System.out.println("RFID ip: " + m_ip + ";天线数：" + Integer.toString(m_antCount) + ";读到标签数：" + tagnum[0]);
+					for (int i = 0; i < tagnum[0]; i++) {
+						try {
+							TAGINFO tagInfo = devReader.new TAGINFO();
+							devReader.GetNextTag(tagInfo);
+							String cardNo = hex(tagInfo.EpcId);
+							System.out.println(m_ip + "读到无源标签：" + cardNo);
+							zkyAsynReader.rfidReaderUtil.settleReadCard(cardNo,false,m_readerInfo);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				}
+				try{
+					Thread.sleep(500);
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+			}*/
+
+			readPassive(m_ip, m_antennaNum);
 		}
 	}
+
+	public static String hex(byte[] bytes) {
+		StringBuilder result = new StringBuilder();
+		for (byte aByte : bytes) {
+			result.append(String.format("%02x", aByte));
+			// upper case
+			// result.append(String.format("%02X", aByte));
+		}
+		return result.toString();
+	}
+
+	private void readPassive(String ip, int antennaNum) {
+		Reader devReader = new Reader();
+		try {
+			if (devReader.InitReader_Notype(ip, antennaNum) != READER_ERR.MT_OK_ERR) {
+				System.out.println("RFID InitReader failed：" + ip);
+				return;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+		int antCount = 0;
+		READER_ERR err;
+		/*
+		 * //检测连接的天线 ConnAnts_ST cast=devReader.new ConnAnts_ST();
+		 * err=devReader.ParamGet(Mtr_Param.MTR_PARAM_READER_CONN_ANTS, cast); if (err
+		 * != READER_ERR.MT_OK_ERR){ System.out.println("RFID ParamGet failed：" +
+		 * err.toString()+"("+ip+")"); return; } if (cast.antcnt < 1){
+		 * System.out.println("RFID ParamGet antcnt is zero!"); return; } antCount =
+		 * cast.antcnt;
+		 */
+		// 改成新的天线测试方法
+		int[] antsTemp = new int[4];
+		for (int i = 1; i <= 4; i++) {
+			AntPortsVSWR apvr = devReader.new AntPortsVSWR();
+			apvr.andid = i;
+			apvr.power = (short) 3000;
+			apvr.region = Region_Conf.RG_NA;
+			// 定频
+			apvr.frecount = 1;
+			apvr.vswrs[0].frequency = 915250;
+			err = devReader.ParamGet(Mtr_Param.MTR_PARAM_RF_ANTPORTS_VSWR, apvr);
+			// antID1234即对应物理天线口1234，当获取的vswr小于10时即可认为天线口有天线连接
+
+			System.out.println(
+					" antID = " + i + " vswr= " + apvr.vswrs[0].vswr + " frequency= " + apvr.vswrs[0].frequency);
+			if (err != READER_ERR.MT_OK_ERR) {
+				System.out.println("RFID ParamGet for andid=" + Integer.toString(i) + " failed：" + err.toString() + "("
+						+ ip + ")");
+			} else {
+				if(apvr.vswrs[0].vswr <10) {
+					antsTemp[antCount] = i;
+					antCount++;
+				}
+
+			}
+		}
+		if (antCount < 1) {
+			System.out.println("RFID ParamGet antCount is zero!");
+			return;
+		}
+		int[] ants = new int[antCount];
+		for (int i = 0; i < antCount; i++) {
+			ants[i] = antsTemp[i];
+		}
+
+		// 不检测天线
+		/*
+		 * int[] val=new int[1]; val[0]=0;
+		 * err=devReader.ParamSet(Mtr_Param.MTR_PARAM_READER_IS_CHK_ANT, val); if (err
+		 * != READER_ERR.MT_OK_ERR){ System.out.println("RFID ParamSet failed：" +
+		 * err.toString()+"("+ip+")"); return; }
+		 *
+		 *
+		 * int[] ants=new int[antennaNum]; for(int i=0;i<antennaNum;i++){ ants[i] = i+1;
+		 * }
+		 */
+		while (true){
+			TAGINFO[] tags = new TAGINFO[200];
+			int[] tagnum = new int[1];
+			short timeout = 1000; // 1秒
+			// err = devReader.TagInventory(cast.connectedants, antCount, timeout, tags,
+			// tagnum);
+			// err = devReader.TagInventory(ants,antCount, timeout, tags, tagnum);
+			// 2022年6月17日,使用新的盘点接口
+			err = devReader.TagInventory_Raw(ants, antCount, timeout, tagnum);
+			if (err != READER_ERR.MT_OK_ERR) {
+				System.out.println("RFID TagInventory failed：" + err.toString() + "(" + ip + ")");
+				return;
+			}
+			if(tagnum[0] > 0){
+				System.out.println("RFID ip: " + ip + ";天线数：" + Integer.toString(antCount) + ";读到标签数：" + tagnum[0]);
+				for (int i = 0; i < tagnum[0]; i++) {
+					try {
+						TAGINFO tagInfo = devReader.new TAGINFO();
+						devReader.GetNextTag(tagInfo);
+						String cardNo = hex(tagInfo.EpcId);
+						System.out.println(ip + "读到无源标签：" + cardNo);
+						zkyAsynReader.rfidReaderUtil.settleReadCard(cardNo,false ,m_readerInfo);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			try{
+				Thread.sleep(500);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		//devReader.CloseReader();
+	}
+
 }
